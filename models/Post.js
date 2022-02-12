e93447d684ff9cb233a5054e6ec62cd3783d6d04
Jason Boudreaux <js.bdrx@gmail.com>
2022-02-11 23:34:25 -0600

    created post-routes.js

--- models/Post.js
+++ models/Post.js
@@ -1,31 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+// create our Post model
+class Post extends Model {}
 
-class Post extends Model {
-  static upvote(body, models) {
-    return models.Vote.create({
-      user_id: body.user_id,
-      post_id: body.post_id
-    }).then(() => {
-      return Post.findOne({
-        where: {
-          id: body.post_id
-        },
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at',
-          [
-            sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-            'vote_count'
-          ]
-        ]
-      });
-    });
-  }
-}
-
+// create fields/columns for Post model
 Post.init(
   {
     id: {
@@ -38,12 +16,9 @@ Post.init(
       type: DataTypes.STRING,
       allowNull: false
     },
-    post_url: {
+    post_body: {
       type: DataTypes.STRING,
       allowNull: false,
-      validate: {
-        isURL: true
-      }
     },
     user_id: {
       type: DataTypes.INTEGER,
